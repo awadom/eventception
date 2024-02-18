32b01583054754365119bcf753877affea54edf1
denfushi <awadomar@gmail.com>
2024-02-18 11:06:03 -0500

    build

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React from "react";
 function App() {
   const readGoogleSheet = () => {
     fetch("https://sheetdb.io/api/v1/cdgmxgjxfvt4d", {
-      method: 'GET',
-      headers: {'Authorization': 'Bearer p5oie4u1hmlyq7xpz3zjfjdmjiamqta2640k3f7l'}
+      method: "GET",
+      headers: {
+        Authorization: "Bearer p5oie4u1hmlyq7xpz3zjfjdmjiamqta2640k3f7l",
+      },
     })
       .then((response) => response.json())
       .then((data) => console.log(data));
